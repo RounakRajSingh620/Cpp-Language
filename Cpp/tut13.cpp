245038dfb1a344cd789cb0bb5c7e905039f6fe68
RounakRajSingh <raunakrajsingh620@gmail.com>
2022-10-08 00:04:55 +0530

    Basics of C++

--- Cpp/tut13.cpp
+++ Cpp/tut13.cpp
@@ -27,8 +27,38 @@ int main(){
     {
         cout<<"The value of marks "<<i <<" is "<<marks[i]<<endl;
     }
-    
 
+    // //Quiz quiz : do the same using while and do-while loops?
+
+    // //while loop
+    
+    // int i=0;
+    // while (i<4)
+    // {
+    //    cout<<"The value of marks "<<i <<" is "<<marks[i]<<endl;
+    //    i++;
+    // }
+
+    // //DO while
+    // int i=0;
+    // do
+    // {
+    //     cout<<"The value of marks "<<i <<" is "<<marks[i]<<endl;
+    //     i++;
+    // } while (i<4);
+
+    // Pointers and arrays
+
+    int *p =marks;
+    cout<<*(p++)<<endl;
+    cout<<*(++p)<<endl;
+
+    // cout<<"The value of *p is "<<*p<<endl;
+    // cout<<"The value of *(p+1) is "<<*(p+1)<<endl;
+    // cout<<"The value of *(p+2) is "<<*(p+2)<<endl;
+    // cout<<"The value of *(p+3) is "<<*(p+3)<<endl;
+    
+    
 
     return 0;
 }
